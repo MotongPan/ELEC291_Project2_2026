84ca05021c80b90f22f5482fa801607869ce599b
MotongPan <panmotonglisa@126.com>
2026-03-21 18:15:57 -0700

    Update adc.c

--- robot/adc.c
+++ robot/adc.c
@@ -4,16 +4,8 @@
 
 void initADC(void)
 {
-	RCC->APB2ENR |= BIT9; // peripheral clock enable for ADC (page 175 or RM0451)
-
-	// ADC clock selection procedure (page 746 of RM0451)
-	/* (1) Select PCLK by writing 11 in CKMODE */
-	ADC1->CFGR2 |= ADC_CFGR2_CKMODE; /* (1) */
-	
-	// ADC enable sequence procedure (page 745 of RM0451)
-	/* (1) Clear the ADRDY bit */
-	/* (2) Enable the ADC */
-	/* (3) Wait until ADC ready */
+	RCC->APB2ENR |= BIT9;
+	ADC1->CFGR2 |= ADC_CFGR2_CKMODE;
 	ADC1->ISR |= ADC_ISR_ADRDY; /* (1) */
 	ADC1->CR |= ADC_CR_ADEN; /* (2) */
 	if ((ADC1->CFGR1 & ADC_CFGR1_AUTOFF) == 0)
@@ -24,12 +16,6 @@ void initADC(void)
 		}
 	}	
 
-	// Calibration code procedure (page 745 of RM0451)
-	/* (1) Ensure that ADEN = 0 */
-	/* (2) Clear ADEN */
-	/* (3) Set ADCAL=1 */
-	/* (4) Wait until EOCAL=1 */
-	/* (5) Clear EOCAL */
 	if ((ADC1->CR & ADC_CR_ADEN) != 0) /* (1) */
 	{
 		ADC1->CR |= ADC_CR_ADDIS; /* (2) */
@@ -44,13 +30,6 @@ void initADC(void)
 
 int readADC(unsigned int channel)
 {
-	// Single conversion sequence code example - Software trigger (page 746 of RM0451)
-	/* (1) Select HSI16 by writing 00 in CKMODE (reset value) */
-	/* (2) Select the auto off mode */
-	/* (3) Select channel */
-	/* (4) Select a sampling mode of 111 i.e. 239.5 ADC clk to be greater than17.1us */
-	/* (5) Wake-up the VREFINT (only for VRefInt) */
-	//ADC1->CFGR2 &= ~ADC_CFGR2_CKMODE; /* (1) */
 	ADC1->CFGR1 |= ADC_CFGR1_AUTOFF; /* (2) */
 	ADC1->CHSELR = channel; /* (3) */
 	ADC1->SMPR |= ADC_SMPR_SMP_0 | ADC_SMPR_SMP_1 | ADC_SMPR_SMP_2; /* (4) */
@@ -67,4 +46,31 @@ int readADC(unsigned int channel)
 	}
 
 	return ADC1->DR; // ADC_DR has the 12 bits out of the ADC
+}
+unsigned int adc_read_left(void)
+{
+    return readADC(ADC_CHSELR_CHSEL0);
+}
+
+unsigned int adc_read_right(void)
+{
+    return readADC(ADC_CHSELR_CHSEL1);
+}
+
+unsigned int adc_read_intersection(void)
+{
+    return readADC(ADC_CHSELR_CHSEL4);
+}
+void ADC_Pin_Init(void)
+{
+    RCC->IOPENR |= BIT0; // GPIOA clock enable
+
+    // PA0 -> analog
+    GPIOA->MODER |= (BIT0 | BIT1);
+
+    // PA1 -> analog
+    GPIOA->MODER |= (BIT2 | BIT3);
+
+    // PA4 -> analog
+    GPIOA->MODER |= (BIT8 | BIT9);
 }
