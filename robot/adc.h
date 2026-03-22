01dba369c72439faebf2df78cf7fcee927c02ad4
MotongPan <panmotonglisa@126.com>
2026-03-21 18:16:13 -0700

    Update adc.h

--- robot/adc.h
+++ robot/adc.h
@@ -1,2 +1,12 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+
+void ADC_Pin_Init(void);
 void initADC(void);
 int readADC(unsigned int channel);
+
+unsigned int adc_read_left(void);
+unsigned int adc_read_right(void);
+unsigned int adc_read_intersection(void);
+
+#endif
